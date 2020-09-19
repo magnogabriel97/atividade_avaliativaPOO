a0e405797e40399d5af346de97a5a99b371fe964
magnogabriel97 <69545669+magnogabriel97@users.noreply.github.com>
2020-09-18 22:35:42 -0300

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,50 @@
+package magno_toniolo.aula20200915.atividade_avaliativa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Time {
+
+    private String nome;
+    private List<Jogador> jogadoresDoTime = new ArrayList<>();
+    private Estadio estadioDoTime;
+    private List<TitulosBrasileiros> titulosBrasileiros = new ArrayList<>();
+
+    public Time(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void addJogador(Jogador novoJogador) {
+        this.jogadoresDoTime.add(novoJogador);
+    }
+
+    public List<Jogador> getJogadoresDoTime() {
+        return jogadoresDoTime;
+    }
+
+    public void addTitulo(TitulosBrasileiros novoTitulo) {
+        this.titulosBrasileiros.add(novoTitulo);
+    }
+
+    public List<TitulosBrasileiros> getTitulosBrasileiros() {
+        return titulosBrasileiros;
+    }
+
+    public Estadio getEstadioDoTime() {
+        return estadioDoTime;
+    }
+
+    public void setEstadioDoTime(Estadio estadioDoTime) {
+        this.estadioDoTime = estadioDoTime;
+    }
+
+    @Override
+    public String toString() {
+        return (nome.toUpperCase());
+    }
+
+}
---
+++ TitulosBrasileiros.java
@@ -0,0 +1,31 @@
+package magno_toniolo.aula20200915.atividade_avaliativa;
+
+public class TitulosBrasileiros {
+    private String titulo;
+    private int quantidadeDeVezesQueGanhou;
+
+    public TitulosBrasileiros(String titulo, int quantidadeDeVezesQueGanhou) {
+        this.titulo = titulo;
+        this.quantidadeDeVezesQueGanhou = quantidadeDeVezesQueGanhou;
+        if (quantidadeDeVezesQueGanhou <= 0) {
+            throw new RuntimeException("Quantidade de vitórias deve ser maior que zero");
+        }
+    }
+
+    public int getQuantidadeDeVezesQueGanhou() {
+        return quantidadeDeVezesQueGanhou;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Campeão do(a) " + titulo + " - " + quantidadeDeVezesQueGanhou + " vezes";
+    }
+}
